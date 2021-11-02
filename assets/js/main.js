d2d19bd930fde7ca684d34187ca785d1b384a27b
iamfrego <mate_freg@tiscali.it>
2021-11-02 15:06:31 +0100

    esercizio base completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,25 @@ const app = new Vue({
     el: `#app`,
     data:{
         newTask:``,
+
         tasks:[
             `Completare l' esercizio`,
             `Iniziare i bonus`,
             `Esempio`
-        ]
+        ],
+
+        compTasks: [
+
+        ],
     },
     methods:{
         removeTask(i){
-            this.tasks.splice(i, 1)
+            this.tasks.splice(i, 1);
+        },
+        completeTask(i) {
+            this.compTasks.push(this.tasks[i]);
+            console.log(this.compTasks);
+            this.tasks.splice(i, 1);
         },
         addTask(){
             if(this.newTask != "" && this.newTask.length > 5){
