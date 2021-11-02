732f445fc967a697962678cf8c3741316b031d70
iamfrego <mate_freg@tiscali.it>
2021-11-02 15:41:39 +0100

    scambio tra le 3 sezioni

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 const app = new Vue({
     el: `#app`,
-    data:{
-        newTask:``,
+    data: {
+        newTask: ``,
 
-        tasks:[
+        tasks: [
             `Completare l' esercizio`,
             `Iniziare i bonus`,
             `Esempio`
@@ -12,23 +12,40 @@ const app = new Vue({
         compTasks: [
 
         ],
+
+        delTasks: [
+
+        ],
     },
-    methods:{
-        removeTask(i){
+    methods: {
+        removeTask(i) {
+            this.delTasks.push(this.tasks[i]);
             this.tasks.splice(i, 1);
         },
+
         completeTask(i) {
             this.compTasks.push(this.tasks[i]);
-            console.log(this.compTasks);
             this.tasks.splice(i, 1);
         },
-        addTask(){
-            if(this.newTask != "" && this.newTask.length > 5){
-            this.tasks.push(this.newTask);
-        }else{
-            alert("Non puoi inserire una task vuota o con meno di 5 caratteri");
+
+        addTask() {
+            if (this.newTask != "" && this.newTask.length > 5) {
+                this.tasks.push(this.newTask);
+            } else {
+                alert("Non puoi inserire una task vuota o con meno di 5 caratteri");
+            }
+            this.newTask = "";
+        },
+
+        redoTask(i) {
+            this.tasks.push(this.compTasks[i]);
+            this.compTasks.splice(i, 1);
+        },
+
+        restoreTask(i) {
+            this.tasks.push(this.delTasks[i]);
+            this.delTasks.splice(i, 1);
         }
-        this.newTask = "";
-        }
+
     }
 })
